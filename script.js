63b99afe764264ecec488a5e6a4b09afe041b73c
Makeswari Ramanathan <mahithamakeswari91@gmail.com>
2025-08-04 08:57:21 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+// part 1 Fizz Buzz
+
+for (let x = 1; x <= 100; x++) {
+  if (x % 3 === 0 && x % 5 === 0) {
+    console.log("Fizz Buzz");
+  } else if (x % 3 === 0) {
+    console.log("Fizz");
+  } else if (x % 5 === 0) {
+    console.log("Buzz");
+  } else {
+    console.log(x);
+  }
+}
